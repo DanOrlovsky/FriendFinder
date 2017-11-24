31604c6b65a64a74406cf0dac7eda208b49e4600
DannyRux <dangorlov@yahoo.com>
2017-11-24 12:40:41 -0500

    Finished up comments

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,27 +1,46 @@
+// ------------------------------------------------------
+// -- API Routes                                       --
+// ------------------------------------------------------
+
+// Require our Friends data
 const Friends = require('../data/friends');
 
 
 module.exports = (app) => {
+    // GET: /api/getfriends
+    // Returns friends data as a json object
     app.get('/api/getfriends', (req, res) => {
         res.json(Friends);
     });
 
+    // POST: /api/compareFriends
+    // Sends a friend object via ajax, compares it with the friends in the database, and returns the closest match.
     app.post('/api/compareFriends', (req, res) => {
-        console.log(req.body);
+        // Grabs the posted data
         let user = req.body;
-        let totalDifferences = 2000;
+        // Set lowestDifference to an extremely high number so even in the event where two users are completely different, lowestDifference will
+        // be set
+        let lowestDifference = 2000;
+        // index of the lowest matching difference.
         let lowestIdx = 0;
+        // Opens a loop through the friends object
         for(var i = 0; i < Friends.length; i++) {
-            let tempDifference = 0;
+            let tempDifference = 0; // Temporary difference
+            // Loops through the users' scores.
             for(var j = 0; j < user.scores.length; j++) {
+                // We are getting the absolute value of the differences, and adding to our temp variable.
                 tempDifference += Math.abs(user.scores[j] - Friends[i].scores[j])
             }
-            if(tempDifference < totalDifferences) { 
+            // Check if this userscore difference is lower than the current lowest
+            if(tempDifference < lowestDifference) { 
+                // If it is, assign the index of the friend and reassign the lowest difference.
                 lowestIdx = i;
-                totalDifferences = tempDifference;
+                lowestDifference = tempDifference;
             }
         }
+        // Add the new user to the friends object
         Friends.push(user);
+        // Send the matching user back to our form.
         res.json(Friends[lowestIdx]);
     });
 };
--- server.js
+++ server.js
@@ -1,14 +1,20 @@
+// ----------------------------------------------
+// -- Server                                   --
+// ----------------------------------------------
+
+// Connect to Express
 const express = require('express');
 const app = express();
 
+// Add the body parser to send Json.
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-
+// Add our routes
 require('./app/routing/htmlRoutes.js')(app);
 require('./app/routing/apiRoutes')(app);
 
-
+// Set up our port and open our server.
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);
